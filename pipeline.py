b34fb9d931741b6880f835c39cd4be65736dedf5
DAT <tdat.nguyen93@gmail.com>
2017-03-01 18:24:31 -0800

    Updated LineTracker

--- pipeline.py
+++ pipeline.py
@@ -1,42 +1,48 @@
 import cv2
 import numpy as np
-from utils import mix_color_grad_thresh, CameraCalibration, ProjectionManager
-from utils import adaptive_equalize_image, weighted_img, region_of_interest
+from utils import mix_color_grad_thresh, adaptive_equalize_image, weighted_img, region_of_interest, draw_windows
+from utils import CameraCalibration, ProjectionManager, LineTracker
 import matplotlib.pyplot as plt
 
-
-# Calibrate camera
-cam_calibration = CameraCalibration(p_file='calibration_mat.p', img_dir=None)
-mtx, dst, img_size = cam_calibration.get()
-row = img_size[0]
-col = img_size[1]
-
 # Read a new image
-img = cv2.imread('./test_images/test2.jpg')
+img = cv2.imread('./test_images/test16.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# Un-distort image
-undst_img = adaptive_equalize_image(cv2.undistort(img, mtx, dst), level=2)
-# Threshold image
-binary_img = mix_color_grad_thresh(undst_img, s_thresh=(180, 255), grad_thresh=(40, 90))
 
-# Perspective Transform
-projmgr = ProjectionManager(row, col, offset=300)
-binary_img = region_of_interest(binary_img, projmgr.get_roi())
-birdeye_img = projmgr.get_birdeye_view(binary_img)
+def process_frame(img):
+    # Calibrate camera
+    cam_calibration = CameraCalibration(p_file='calibration_mat.p', img_dir=None)
+    mtx, dst, img_size = cam_calibration.get()
+    row = img_size[0]
+    col = img_size[1]
 
-# Lane Fitting and Tracking
+    # Un-distort image
+    undst_img = adaptive_equalize_image(cv2.undistort(img, mtx, dst), level=2)
+    # Threshold image
+    binary_img = mix_color_grad_thresh(undst_img, s_thresh=(180, 255), grad_thresh=(40, 90))
 
+    # Perspective Transform
+    projmgr = ProjectionManager(row, col, offset=300)
+    binary_img = region_of_interest(binary_img, projmgr.get_roi())
+    birdeye_img = projmgr.get_birdeye_view(binary_img)
 
-# Plot the result
-f, ax = plt.subplots(2, 2)
-f.tight_layout()
-ax[0, 0].imshow(img)
-ax[0, 0].set_title('Original Image')
-ax[0, 1].imshow(undst_img)
-ax[0, 1].set_title('Undistorted image')
-ax[1, 0].imshow(binary_img, cmap='gray')
-ax[1, 0].set_title('Thresholded and ROI Cropped')
-ax[1, 1].imshow(birdeye_img, cmap='gray')
-ax[1, 1].set_title('Bird Eye View')
+    # Lane Fitting and Tracking
+    curve_centers = LineTracker(window_height=80, window_width=25, margin=10, ym=10/720, xm=4/384, smooth_factor=15)
+    window_centroids = curve_centers.find_lane_line(warped=birdeye_img)
+    lane_lines = draw_windows(birdeye_img, w=25, h=80, window_centroids=window_centroids)
+
+    # Convert back to normal view
+    # Plot the result
+    f, ax = plt.subplots(2, 2)
+    f.tight_layout()
+    ax[0, 0].imshow(img)
+    ax[0, 0].set_title('Original Image')
+    ax[0, 1].imshow(undst_img)
+    ax[0, 1].set_title('Undistorted image')
+    ax[1, 0].imshow(birdeye_img, cmap='gray')
+    ax[1, 0].set_title('Thresholded and ROI Cropped')
+    ax[1, 1].imshow(window_img, cmap='gray')
+    ax[1, 1].set_title('Bird Eye View')
+
+process_frame(img)
 plt.show()
--- utils/LineTracker.py
+++ utils/LineTracker.py
@@ -1,8 +1,92 @@
 import cv2
 import numpy as np
-
+from utils.mask_roi import weighted_img
 
 class LineTracker(object):
-    def __init__(self):
-        self.window_width = None
-        self.window_height = None
+    def __init__(self, window_width, window_height, margin, xm, ym, smooth_factor):
+        self.window_width = window_width
+        self.window_height = window_height
+        self.margin = margin
+        self.ym_per_pixel = ym
+        self.xm_per_pixel = xm
+        self.smooth_factor = smooth_factor
+        self.recent_centers = []           # Store recent windows (left,right)
+
+    def find_lane_line(self, warped):
+        wd_w = self.window_width
+        wd_h = self.window_height
+        margin = self.margin
+        r = warped.shape[0]
+        c = warped.shape[1]
+        wd_centroids = []   # store (left, right) windows centroid position per level
+        windows = np.ones(wd_w)  # Window Template for convolution
+
+        # First find the two staring positions for the left and right lane using np.sum
+        # to get the vertical image slice.
+
+        # Then, use np.convolve the vertical image slice with the window template
+
+        # Sum quarter bottom of image to get slice, could use a different ratio
+        l_sum = np.sum(warped[int(3*r/4):, :int(c/2)], axis=0)
+        l_center = np.argmax(np.convolve(windows, l_sum)) - wd_w/2
+        r_sum = np.sum(warped[int(3*r/4):, int(c/2):], axis=0)
+        r_center = np.argmax(np.convolve(windows, r_sum)) - wd_w/2 + int(c/2)
+
+        # Add what we found for the first layer
+        wd_centroids.append((l_center, r_center))
+
+        # Iterate through each layer looking for max pixel locations
+        for level in range(1, int(r/wd_h)):
+            # convolve the window into the vertical slice of the image
+            img_layer  = np.sum(warped[int(r - (level+1)*wd_h):int(r-level*wd_h), :], axis=0)
+            conv_signal = np.convolve(windows, img_layer)
+            # Find the est left centroid using past left center as a reference
+            # Use window_width/2 as offset because convolution signal reference is at the right side of window,
+            # not the center of the window
+            offset = wd_w/2
+            l_min_index = int(max(l_center+offset-margin, 0))
+            l_max_index = int(min(l_center+offset+margin, c))
+            l_center = np.argmax(conv_signal[l_min_index:l_max_index]) + l_min_index - offset
+
+            # Find the  best right centroid
+            r_min_index = int(max(r_center+offset-margin, 0))
+            r_max_index = int(min(r_center+offset+margin, c))
+            r_center = np.argmax(conv_signal[r_min_index:r_max_index]) + r_min_index - offset
+
+            # Update window_centroids
+            wd_centroids.append((l_center, r_center))
+
+        self.recent_centers.append(wd_centroids)
+
+        # Smooth the line
+        return np.average(self.recent_centers[-self.smooth_factor:], axis=0)
+
+
+def window_mask(width, height, img, center, level):
+    row = img.shape[0]
+    col = img.shape[1]
+    output = np.zeros_like(img)
+    output[int(row - (level+1)*height):int(row - level*height), max(0, int(center-width)):min(int(center+width), col)] = 1
+    return output
+
+
+def draw_windows(img, w, h, window_centroids):
+    left_pts = np.zeros_like(img)
+    right_pts = np.zeros_like(img)
+
+    for level in range(0, len(window_centroids)):
+        # Draw window
+        l_mask = window_mask(w, h, img, window_centroids[level][0], level)
+        r_mask = window_mask(w, h, img, window_centroids[level][1], level)
+        # Add graphic points to window mask
+        left_pts[(left_pts == 255) | (l_mask == 1)] = 255
+        right_pts[(right_pts == 255) | (r_mask == 1)] = 255
+
+    # Draw result
+    template = np.array(left_pts+right_pts, dtype='uint8')
+    zero_channel = np.zeros_like(template)
+    template = np.array(cv2.merge((zero_channel, template, zero_channel)), dtype='uint8')
+    warpage = np.array(cv2.merge((img, img, img)), dtype='uint8')
+    result = weighted_img(warpage, template,α=1.0, β=0.3, λ=0.)
+
+    return result
